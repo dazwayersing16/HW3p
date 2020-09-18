e83f7d43d8689983f93c7a63f2d389f8bba85551
dazwayersing16 <ajw6150@psu.edu>
2020-09-18 21:10:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def digit_sum(n):
+  if n/10 == 0:
+    return 0
+  else:
+    return int(n%10 + digit_sum((n - n%10)/10))
+
+def run():
+  num = int(input("Enter an int: "))
+  print("sum of digits of {} is {}.".format(num,digit_sum(num)))
+
+if __name__ == '__main__':
+  run() 
